43635743c2bb25fc03aeef50f1b0562e518626ca
nileshsingh72 <rnileshchauhan89@gmail.com>
2022-12-07 12:13:29 +0530

    updated

--- Config/connect.js
+++ Config/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   return mongoose.connect(
-    "mongodb+srv://mongo:mongo@cluster0.7ydz8wg.mongodb.net/at?retryWrites=true&w=majority"
+    "mongodb+srv://nilesh:nilesh@cluster0.hnixegw.mongodb.net/at?retryWrites=true&w=majority"
   );
 };
 
--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const connect = require("./Config/connect.js");
 const express = require("express");
 const cors = require("cors");
 const ProjectRoute = require("./Features/Project/project.route.js");
+const BlogRoute = require("./Features/Blog/blog.route.js");
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 app.use("/project", ProjectRoute);
+app.use("/blog", BlogRoute);
 app.listen(8001, async () => {
   try {
     await connect();
     console.log("data connected");
   } catch (err) {
-    console.log(err.message);
+    console.log(err.message, "not connected");
   }
 
   console.log(`listening on http://localhost:8001`);
